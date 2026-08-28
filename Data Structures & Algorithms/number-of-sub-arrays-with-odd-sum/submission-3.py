5f7beb57160a744ad9ebad305824daca965cfb51
faaaisalsk <fhsk019@gmail.com>
2026-08-28 23:37:22 +0530

    Add: number-of-sub-arrays-with-odd-sum - submission-3

---
+++ Data Structures & Algorithms/number-of-sub-arrays-with-odd-sum/submission-3.py
@@ -0,0 +1,18 @@
+class Solution:
+    def numOfSubarrays(self, arr: List[int]) -> int:
+        curr_sum = 0
+        odd_cnt = 0
+        even_cnt = 0
+        res = 0
+        mod = 10**9 + 7
+        #revise
+        for n in arr:
+            curr_sum +=n
+
+            if curr_sum %2:
+                res = (res+1 + even_cnt) % mod
+                odd_cnt +=1
+            else:
+                res = (res + odd_cnt) % mod
+                even_cnt +=1
+        return res
