cd468e3d836c6f33cd0dd1b9938643341071585d
faaaisalsk <fhsk019@gmail.com>
2026-09-10 20:31:46 +0530

    Add: minimum-remove-to-make-valid-parentheses - submission-2

---
+++ Data Structures & Algorithms/minimum-remove-to-make-valid-parentheses/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        res = []
+        cnt = 0 #count extra ( parantheses
+        for c in s:
+            if c == '(':
+                res.append(c)
+                cnt +=1
+            elif c == ')' and cnt > 0:
+                res.append(c)
+                cnt -=1
+            elif c != ')':
+                res.append(c)
+        
+        filtered = []
+        for n in res[::-1]:
+            if n == '(' and cnt > 0:
+                cnt -=1
+            else:
+                filtered.append(n)
+        return "".join(filtered[::-1])
